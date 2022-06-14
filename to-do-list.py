9ea16340da56559e639afa035a82bc3da12e4274
jnchen12a <jnchen12a@gmail.com>
2022-06-14 12:37:05 -0500

    Completed portion of loop that completes tasks and removed some small bugs.

--- to-do-list.py
+++ to-do-list.py
@@ -9,8 +9,11 @@ def show_tdlist():
         print(" - " + item)
     print()
     print("Completed:")
-    for item in completed:
-        print(" - " + item)
+    if len(completed) == 0:
+        print("None (yet)")
+    else:
+        for item in completed:
+            print(" - " + item)
     print()
 
 #Seeing if user is worthy of using this program
@@ -55,10 +58,49 @@ while True:
     #if else statement that uses input
     if decision == "add": #adding a task
         newitem = input("What task would you like to add?")
+        newitem = newitem.capitalize()
+        if newitem in unfinished:
+            print("Sorry, that item is already there.")
+            show_tdlist()
+            continue
         unfinished.append(newitem)
         show_tdlist()
-    elif decision == "complete":
-        #Stuff
+    elif decision == "complete": #completing a task
+        if len(unfinished) == 0:#Bozo check
+            print("Sorry. There seems to be no tasks to complete, but that might be a good thing.")
+            continue
+
+        print("Congratulations! Which task did you complete?")
+
+        #Lists out all unfinished tasks
+        for item in unfinished:
+            tasknumber = unfinished.index(item) + 1
+            tasknumberstr = str(tasknumber)
+            print(tasknumberstr + ". " + item)
+
+        #Asks which one was completed
+        while True:
+            completedtaskn = input("Please enter the number corresponding to the task you completed or type 0 to return to the previous menu.")
+            try:
+                completedtaskn = int(completedtaskn)
+            except:
+                print("Please enter a number. Try again.")
+                continue
+            
+            #if else statement to decide what to do next
+            if completedtaskn == 0: #return to previous menu
+                break
+            elif completedtaskn > len(unfinished): #Bozo check
+                print("That task doesn't exist yet. Try again.")
+                continue
+            else: #Move task from unfinished to completed list
+                completedtaskn = completedtaskn - 1
+                movingtask = unfinished[completedtaskn]
+                del unfinished[completedtaskn]
+                completed.append(movingtask)
+                print("Nice! That task has been officially completed!")
+                show_tdlist()
+                break
     elif decision == "quit": #quiting out of program
         print("Thank you for using this program! Three cheers to you for being productive today!")
         quit()
